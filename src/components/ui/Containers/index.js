9756b96bf76f3d17c32e996405e39a1c601ce049
kozerka <kozerka@gmail.com>
2023-11-28 12:18:03 +0100

    Reusable SectionTitle component

--- src/components/ui/Containers/index.js
+++ src/components/ui/Containers/index.js
@@ -4,3 +4,4 @@ export * from './PageContainer.styled';
 export * from './SelectContainer.styled';
 export * from './DashboardContainer.styled';
 export * from './ButtonContainer.styled';
+export * from './SectionContainer.styled';
--- src/pages/private/AllNotes/AllNotes.js
+++ src/pages/private/AllNotes/AllNotes.js
@@ -1,5 +1,7 @@
+import { SectionContainer } from '../../../components/ui/Containers';
+
 const AllNotes = () => {
-	return <div>AllNotes</div>;
+	return <SectionContainer>AllNotes</SectionContainer>;
 };
 
 export default AllNotes;
