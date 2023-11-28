3ef9f91ec8d4339b3402a7724eb0bbb6deddbcff
kozerka <kozerka@gmail.com>
2023-11-28 15:49:07 +0100

    Work postponed until merger with BE

--- src/pages/private/AllTutorConnections/AllTutorConnections.js
+++ src/pages/private/AllTutorConnections/AllTutorConnections.js
@@ -1,5 +1,6 @@
 const AllTutorConnections = () => {
 	return <div>AllTutorConnections</div>;
 };
+// TODO dopiero po polaczeniu a API-  pobieranie przez map tablicy spotkan renderowanie w komponencie karty
 
 export default AllTutorConnections;
